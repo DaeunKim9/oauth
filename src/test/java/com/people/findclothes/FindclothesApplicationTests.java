5e3fb135e3a76ab325f11f542f7207bf4dea4ed3
chy0503 <202015006@kyonggi.ac.kr>
2024-02-03 20:14:47 +0900

    Initial commit

---
+++ src/test/java/com/people/findclothes/FindclothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.findclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindclothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
